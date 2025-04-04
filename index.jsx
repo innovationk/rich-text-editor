0c858bf946f426c868d3d3118aff0356728edac1
arduored <edouard.souan@zohomail.eu>
2025-04-04 16:34:07 +0200

    Chore: add base to display package

---
+++ index.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./lib/app";
+
+const rootNode = document.getElementById("root");
+const root = createRoot(rootNode, {
+  onCaughtError: (error) => {
+    console.error(error);
+  },
+});
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
---
+++ lib/app.jsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div>app to test the package</div>;
+}

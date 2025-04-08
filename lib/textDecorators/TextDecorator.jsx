d615361fbba28f7e02ca2262abb6ca8aac8a4ce1
arduored <edouard.souan@zohomail.eu>
2025-04-08 08:31:13 +0200

    Chore: add JSDoc types

--- lib/textDecorators/TextDecorator.jsx
+++ lib/textDecorators/TextDecorator.jsx
@@ -1,5 +1,10 @@
 import { Children } from "react";
 
+/**
+ * 
+ * @param {TextDecoratorProps} TextDecoratorProps - {@link TextDecoratorProps}
+ * @returns JSX Element
+ */
 export default function TextDecorator({ cb, children }) {
   function handleClick(e) {
     e.preventDefault();
@@ -11,3 +16,9 @@ export default function TextDecorator({ cb, children }) {
     </button>
   );
 }
+
+/**
+ * @typedef {Object} TextDecoratorProps
+ * @property {function} cb - The callback to be called on click event
+ * @property {Array} children - Array of ReactNode to display inside the button
+ */

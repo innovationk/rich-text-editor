4df7a66c52805c6d1f848129501ade25d98415e6
Kim SAVAROCHE <ksavaroche@innovationk.fr>
2025-04-09 13:10:24 +0200

    init

--- index.js
+++
@@ -1,5 +0,0 @@
-import RichText from "./lib/richTextEditor/RichText";
-import { RichTextRow } from "./lib/richTextEditor/RichTextRow";
-import { RichTextBlock } from "./lib/richTextEditor/RichTextBlock";
-
-export { RichText, RichTextRow, RichTextBlock };

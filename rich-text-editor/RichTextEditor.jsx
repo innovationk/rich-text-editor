6fd1a95af582d5fec5df79ed85f09ef1b2750b01
Kim SAVAROCHE <ksavaroche@innovationk.fr>
2025-04-09 14:12:49 +0200

    add css in module for export link

--- rich-text-editor/RichTextEditor.jsx
+++ rich-text-editor/RichTextEditor.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './RichTextEditor.module.css';
 
 const RichTextEditor = () => {
-    return <div>RichTextEditor</div>;
+    return <div className={styles.richtexteditor}>RichTextEditor</div>;
 };
 
 export default RichTextEditor;

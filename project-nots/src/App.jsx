0c918fb5321c0523ebc83d9f0d782502f2ac7ce9
Kim SAVAROCHE <ksavaroche@innovationk.fr>
2025-04-09 13:40:12 +0200

    min vite

---
+++ project-nots/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+function App() {
+
+    return (
+        <div id="app">
+            <h1>
+                Rich Text
+            </h1>
+        </div>
+    )
+}
+
+export default App
